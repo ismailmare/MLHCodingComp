73bd20ccd6089380d484bf9e45503ec3e24afbda
ismailmare <imare@ualberta.ca>
2018-01-07 16:01:02 -0700

    Current supported menus added

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import get_template
+import requests,json
 
 def index(request):
     return render(request, "index.html", {})
@@ -9,4 +10,16 @@ def about(request):
     return render(request, "about.html", {})
 
 def campus(request):
+    url = "https://pos.globalfoodsoft.com/pos/menu"
+
+    headers = {
+        'authorization': "1yb9CBnkH4bv58xMo",
+        'accept': "application/json",
+        'glf-api-version': "2"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+
+
+
     return render(request, "campus.html", {})
